7aeb56d745c1c9fff96ce80e39c6b72252936de4
Dimm <slim.slk@gmail.com>
2024-05-20 21:41:25 +0200

    added Category model and add new migration

--- backend/community_app/models/questions.py
+++ backend/community_app/models/questions.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, UTC
 
 from community_app import db
 
@@ -7,7 +7,8 @@ class Question(db.Model):
     __tablename__ = "questions"
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(UTC))
 
     responses = db.relationship("Response", backref="question", lazy=True)
 
@@ -22,6 +23,15 @@ class Statistic(db.Model):
     disagree_count = db.Column(db.Integer, nullable=False, default=0)
 
     def __str__(self):
-        return "Statistic of question: {}:\n {} of agree\n {} of disagree".format(
+        return "Statistic of question: {}:\n{} of agree\ns{} of disagree".format(
             self.question_id, self.agree_count, self.disagree_count
         )
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __str__(self):
+        return f"Category ID - {self.id}\nCategory name - {self.name}"
